4ba50001ff6c248e98aef6a378fabb5ed06ae2b2
sinansaricayir <sinansaricayir@gmail.com>
2023-04-17 02:54:01 +0300

    Project Starter Settings

---
+++ pages/home/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <div className="font-dancing text-primary">Home Sayfası</div>;
+};
+
+export default index;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ffbe33",
+        secondary: "#222831",
+      },
+      fontFamily: {
+        dancing: ["Dancing Script", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
