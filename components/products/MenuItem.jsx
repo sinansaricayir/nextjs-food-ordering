dc7c1c1c64b8e3bb07dd1726b38dfab8ed7f2683
sinansaricayir <sinansaricayir@gmail.com>
2023-05-04 22:25:22 +0300

    Redux Toolkit  Installation | Product Extras

--- components/products/MenuItem.jsx
+++ components/products/MenuItem.jsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const MenuItem = ({ image, title, text, price }) => {
   return (
     <div className=" bg-secondary text-white shadow-2xl shadow-black rounded-2xl">
-      <div className="bg-[#f1f2f3] w-full h-[210px] grid place-content-center rounded-bl-[30px] rounded-t-xl">
-        <Image
-          className="hover:scale-110 transition-all duration-500"
-          src={image}
-          width={150}
-          height={150}
-          alt=""
-        />
-      </div>
+      <Link href="/product">
+        <div className="bg-[#f1f2f3] w-full h-[210px] grid place-content-center rounded-bl-[30px] rounded-t-xl">
+          <Image
+            className="hover:scale-110 transition-all duration-500"
+            src={image}
+            width={150}
+            height={150}
+            alt=""
+          />
+        </div>
+      </Link>
       <div className="h-40">
         <h4 className="mx-2 mb-2 mt-6 text-xl font-bold">{title}</h4>
         <p className="text-sm mx-2 font-thin">{text}</p>
--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -1,18 +1,53 @@
 import Title from "@/components/ui/Title";
 import Image from "next/image";
+import { useState } from "react";
+
+const extraItems = [
+  {
+    id: 1,
+    name: "ketçap",
+    price: 1,
+  },
+  {
+    id: 2,
+    name: "acı sos",
+    price: 2,
+  },
+  {
+    id: 3,
+    name: "mayonez",
+    price: 1,
+  },
+  {
+    id: 4,
+    name: "hardal",
+    price: 2,
+  },
+];
 
 const Index = () => {
+  const [price, setPrice] = useState(10);
+  const [extras, setExtras] = useState(extraItems);
+
+  const handleExtra = (checked, extraPrice) => {
+    if (checked == true) {
+      setPrice(price + extraPrice);
+    } else {
+      setPrice(price - extraPrice);
+    }
+  };
+
   return (
     <>
-      <div className="h-screen flex items-center justify-center gap-20 flex-wrap">
-        <div className="relative h-[45%] max-w-[600px] min-w-[400px] lg:mt-0 mt-4">
+      <div className="h-screen flex items-center justify-center gap-20 md:flex-row flex-col">
+        <div className="relative md:h-[45%] md:w-[35%] h-[50%] w-[50%] lg:mt-0 mt-4">
           {/* <img src="/images/f1.png" alt="" className="h-full w-full" /> */}
           <Image src="/images/f1.png" alt="" fill />
         </div>
         <div className="flex flex-col lg:items-start items-center">
           <Title className="text-6xl mb-8">Good Pizza</Title>
           <span className="text-primary underline text-2xl font-bold inline-block mb-8 hover:scale-110 transition-all">
-            $10
+            ${price}
           </span>
           <p className="sm:max-w-2xl max-w-xs mb-8">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
@@ -36,7 +71,7 @@ const Index = () => {
               </div>
               <div className="relative h-16 w-16 hover:scale-110 transition-all cursor-pointer">
                 <Image src="/images/size.png" alt="" fill />
-                <span className="absolute top-1 -right-4 text-xs bg-primary text-black font-bold px-1 rounded-xl">
+                <span className="absolute top-1 -right-1 text-xs bg-primary text-black font-bold px-1 rounded-xl">
                   Large
                 </span>
               </div>
@@ -45,18 +80,19 @@ const Index = () => {
           <div className="mb-8">
             <h4 className="font-bold mb-2">Choose Additional Ingredients</h4>
             <div className="flex items-center gap-4">
-              <label className="flex items-center gap-1 cursor-pointer">
-                <input type="checkbox" className="accent-primary peer" />
-                <span className="peer-checked:font-semibold">ketçap</span>
-              </label>
-              <label className="flex items-center gap-1 cursor-pointer">
-                <input type="checkbox" className="accent-primary peer" />
-                <span className="peer-checked:font-semibold">mayonez</span>
-              </label>
-              <label className="flex items-center gap-1 cursor-pointer">
-                <input type="checkbox" className="accent-primary peer" />
-                <span className="peer-checked:font-semibold">acı sos</span>
-              </label>
+              {extras.map((extra) => (
+                <label
+                  className="flex items-center gap-1 cursor-pointer"
+                  key={extra.id}
+                  onChange={(e) => handleExtra(e.target.checked, extra.price)}
+                >
+                  <input type="checkbox" className="accent-primary peer" />
+                  <span className="peer-checked:font-semibold">
+                    {extra.name}{" "}
+                    <span className="text-xs font-bold text-primary">{`(+${extra.price}$)`}</span>
+                  </span>
+                </label>
+              ))}
             </div>
           </div>
           <button className="btn-primary mb-8">Add To Cart</button>
